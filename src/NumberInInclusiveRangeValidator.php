21adf947241bc2b3ff4503042ed04418214ad4d4
SimonIsMe <szym1991@gmail.com>
2017-07-14 23:17:43 +0200

    NumberInInclusiveRangeValidator

---
+++ src/NumberInInclusiveRangeValidator.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Validator;
+
+class NumberInInclusiveRangeValidator implements ValidatorInterface
+{
+	/**
+	 * @var int
+	 */
+	const VALUE_FIT_TO_RANGE = 0;
+
+	/**
+	 * @var int
+	 */
+	const VALUE_TOO_SMALL = 1;
+
+	/**
+	 * @var int
+	 */
+	const VALUE_TOO_BIG = 2;
+
+	/**
+	 * @var double
+	 */
+	private $leftBoundary;
+
+	/**
+	 * @var double
+	 */
+	private $rightBoundary;
+
+	/**
+	 * @param int | double $leftBoundary
+	 * @param int | double $rightBoundary
+	 */
+	public function __construct($leftBoundary, $rightBoundary)
+	{
+		$this->leftBoundary = (double) $leftBoundary;
+		$this->rightBoundary = (double) $rightBoundary;
+	}
+
+	/**
+	 * @param mixed $value
+	 *
+	 * @return int -   0 if value is in specified range (inclusive)
+	 *                 1 if value is too small
+	 *                 2 if value is too big
+	 */
+	public function valid($value): int
+	{
+		if ($value < $this->leftBoundary)
+		{
+			return self::VALUE_TOO_SMALL;
+		}
+
+		if ($this->rightBoundary < $value)
+		{
+			return self::VALUE_TOO_BIG;
+		}
+
+		return self::VALUE_FIT_TO_RANGE;
+	}
+}
