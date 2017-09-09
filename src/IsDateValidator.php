fbfe7134367bcdc9b29eae01c3933ce39f721f96
SimonIsMe <szym1991@gmail.com>
2017-09-09 13:49:33 +0200

    Add errorTexts() method IsDateValidator

--- src/IsDateValidator.php
+++ src/IsDateValidator.php
@@ -24,6 +24,23 @@ class IsDateValidator implements ValidatorInterface
 	}
 
 	/**
+	 * @param int $validationResult
+	 *
+	 * @return string
+	 */
+	public function errorText(int $validationResult): string
+	{
+		switch ($validationResult) {
+			case self::VALUE_IS_DATE:
+				return 'Ok';
+			case self::VALUE_IS_NOT_DATE:
+				return 'Given date is not in correct format.';
+		}
+
+		return '';
+	}
+
+	/**
 	 * @param mixed $value
 	 *
 	 * @return int  - returns 0 if value is correct date
--- tests/unit/IsDateValidatorTest.php
+++ tests/unit/IsDateValidatorTest.php
@@ -15,11 +15,12 @@ class IsDateValidatorTest extends TestCase
 	/**
 	 * @dataProvider dataProvider
 	 */
-	public function test($value, $expectedResult)
+	public function test($value, $expectedResult, $expectedErrorMessage)
 	{
 		$validator = new IsDateValidator();
 		$result = $validator->valid($value);
 		$this->assertEquals($expectedResult, $result);
+		$this->assertEquals($expectedErrorMessage, $validator->errorText($result));
 	}
 
 	/**
@@ -31,58 +32,72 @@ class IsDateValidatorTest extends TestCase
 			[
 				'17-07-02',
 				IsDateValidator::VALUE_IS_DATE,
+				'Ok'
 			],
 			[
 				'2012-12-12',
 				IsDateValidator::VALUE_IS_DATE,
+				'Ok'
 			],
 			[
 				'2012-02-28',
 				IsDateValidator::VALUE_IS_DATE,
+				'Ok'
 			],
 			[
 				'2016-02-29',
 				IsDateValidator::VALUE_IS_DATE,
+				'Ok'
 			],
 			[
 				'2017-02-29',
 				IsDateValidator::VALUE_IS_NOT_DATE,
+				'Given date is not in correct format.'
 			],
 			[
 				'2017-02-62',
 				IsDateValidator::VALUE_IS_NOT_DATE,
+				'Given date is not in correct format.'
 			],
 			[
 				'2017-17-02',
 				IsDateValidator::VALUE_IS_NOT_DATE,
+				'Given date is not in correct format.'
 			],
 			[
 				'2017-7-02',
 				IsDateValidator::VALUE_IS_NOT_DATE,
+				'Given date is not in correct format.'
 			],
 			[
 				'2017-07-2',
 				IsDateValidator::VALUE_IS_NOT_DATE,
+				'Given date is not in correct format.'
 			],
 			[
 				'123',
 				IsDateValidator::VALUE_IS_NOT_DATE,
+				'Given date is not in correct format.'
 			],
 			[
 				123,
 				IsDateValidator::VALUE_IS_NOT_DATE,
+				'Given date is not in correct format.'
 			],
 			[
 				123.456,
 				IsDateValidator::VALUE_IS_NOT_DATE,
+				'Given date is not in correct format.'
 			],
 			[
 				true,
 				IsDateValidator::VALUE_IS_NOT_DATE,
+				'Given date is not in correct format.'
 			],
 			[
 				'',
 				IsDateValidator::VALUE_IS_NOT_DATE,
+				'Given date is not in correct format.'
 			],
 		];
 	}
